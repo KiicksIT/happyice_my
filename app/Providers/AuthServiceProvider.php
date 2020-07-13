ae2246b28e4087c74e6c678fe5b45078f3a029e4
Brian Lee Hong Jie <brian@Brians-MacBook-Air.local>
2020-07-13 12:08:14 +0800

    temp disable auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use App\Permission;
+// use App\Permission;
 //
 
 class AuthServiceProvider extends ServiceProvider
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
+
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
@@ -26,18 +27,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(GateContract $gate)
     {
+/*
         parent::registerPolicies($gate);
         foreach ($this->getPermissions() as $permission) {
             $gate->define($permission->name, function($user) use ($permission){
                 return $user->hasRole($permission->roles);
             });
-        }
+        } */
 
     }
-
+/*
     protected function getPermissions()
     {
         return Permission::with('roles')->get();
     }
-
+ */
 }
